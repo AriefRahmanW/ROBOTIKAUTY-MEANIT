956cfd974ef0a59df19d47b49472b7ab5db79174
MagerNgoding <55614264+MagerNgoding@users.noreply.github.com>
2020-02-29 07:33:10 +0700

    Update test_connection.ino

--- iot/test_connection.ino
+++ iot/test_connection.ino
@@ -1,11 +1,14 @@
 #include <ESP8266WiFi.h>
 
+const char* ssid = "***";
+const char* password = "***";
+
 void setup() {
   Serial.begin(115200);
   
   WiFi.begin(ssid, password);
   
-  Serial.println("Connecting");
+  Serial.println("menghubungkan");
   while(WiFi.status() != WL_CONNECTED) { 
     delay(500);
     Serial.print(".");
@@ -13,8 +16,11 @@ void setup() {
 
 
   Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.print("terhubung dengan IP Address: ");
   Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.print("MAC : ");
+  Serial.println(WiFi.macAddress());
 
 }
 
